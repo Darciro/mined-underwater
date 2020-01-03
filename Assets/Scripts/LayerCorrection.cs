9f9e98f01e0134991d9af5d0d63db0f76edee793
Ricardo Carvalho <rickmanu@gmail.com>
2020-01-03 15:40:11 +0100

    Layer correction for 3D object to show on 2D cameras

---
+++ Assets/Scripts/LayerCorrection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerCorrection : MonoBehaviour
+{
+    public string sortingLayerName = "Default";
+    public int sortingOrder = 0;
+    
+    void Start ()
+    {
+        gameObject.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingLayerName = sortingLayerName;
+        gameObject.GetComponent<ParticleSystem> ().GetComponent<Renderer> ().sortingOrder = sortingOrder;
+        
+    }
+    
+}
